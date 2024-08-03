3d7d1c1f722026284af8e03ef6888e132288c67a
kshenoy25 <54074703+kshenoy25@users.noreply.github.com>
2024-08-03 12:47:12 -0500

    BroCode DS and Algo

--- src/CodingWithBro/packageBigOnotation/Main.java
+++ src/CodingWithBro/packageBigOnotation/Main.java
@@ -13,4 +13,36 @@ public class Main {
     // O(n)
     // O(log n)
     // O(n^2)
+
+    // O (1) constant time:
+    // random access of an element in an array
+    // inserting at the beginning of the LinkedList
+
+    // O (log n) logarithmic time
+    // binary search
+
+    // O(n) linear time
+    // looping through elements in an array
+    // searching through a LinkedList
+
+    // O (n log n) quasilinear
+    // quick sort
+    // merge sort
+    // heapsort
+
+    // O (n^2) quadratic time
+    // insertion sort
+    // selection sort
+    // bubble sort
+
+    // O (n!) factorial time
+    // traveling salesmen problem
+
+    // Grading scale from best to worst run-time
+    // constant time
+    // logarithmic time
+    // linear time
+    // quasilinear time
+    // quadratic time
+    // factorial time
 }
